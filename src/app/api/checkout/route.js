d030ea6a70281eaed65ed8e166f0c1a7664bba95
MatasZos <B00163194@mytudublin.ie>
2025-12-05 12:59:18 +0000

    fixing saving orders

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,8 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 export async function POST(req) {
-  const { searchParams } = new URL(req.url);
-  const email = searchParams.get('email');
+  const { email, items } = await req.json();
 
   const uri = "mongodb+srv://root:myPassword123@cluster0.dsxawqy.mongodb.net/?appName=Cluster0";
   const client = new MongoClient(uri);
@@ -11,10 +10,12 @@ export async function POST(req) {
   const db = client.db('app');
   const orders = db.collection('Orders');
 
-  await orders.updateMany(
-    { userEmail: email, status: "pending" },
-    { $set: { status: "confirmed" } }
-  );
+  await orders.insertOne({
+    userEmail: email,
+    items,
+    createdAt: new Date(),
+    status: "confirmed"
+  });
 
-  return Response.json({ message: "Order confirmed!" });
+  return Response.json({ data: "Order saved" });
 }
