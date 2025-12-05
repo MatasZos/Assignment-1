cf2f8adbb1c1bc914c5d15df53e15d2c20e89be4
MatasZos <B00163194@mytudublin.ie>
2025-12-05 14:40:40 +0000

    update checkout logic

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -15,17 +15,16 @@ export async function POST(req) {
   const cartItems = await carts.find({ userEmail: email }).toArray();
 
   if (cartItems.length > 0) {
-  const items = cartItems.flatMap(c => c.items);
-  const orderTotal = items.reduce((sum, item) => sum + (item.price || 0), 0);
+    const items = cartItems.flatMap(c => c.items);
+    const orderTotal = items.reduce((sum, item) => sum + (item.price || 0), 0);
 
-  const result = await orders.insertOne({
-    userEmail: email,
-    items,
-    total: orderTotal,
-    createdAt: new Date(),
-    status: "confirmed"
-  });
-
+    await orders.insertOne({
+      userEmail: email,
+      items,
+      total: orderTotal,
+      createdAt: new Date(),
+      status: "confirmed"
+    });
 
     await carts.deleteMany({ userEmail: email });
 
